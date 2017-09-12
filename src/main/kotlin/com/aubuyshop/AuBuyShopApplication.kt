042866d0fe565c76a0d996626bacefe8f2d224df
jimagic <jimagicli@163.com>
2017-09-12 22:54:56 +0800

    Initial commit

---
+++ src/main/kotlin/com/aubuyshop/AuBuyShopApplication.kt
@@ -0,0 +1,11 @@
+package com.aubuyshop
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AuBuyShopApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(AuBuyShopApplication::class.java, *args)
+}
---
+++ src/test/kotlin/com/aubuyshop/AuBuyShopApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.aubuyshop
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class AuBuyShopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
